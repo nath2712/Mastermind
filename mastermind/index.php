894cb58eb921b177410a0e0532a011412c8d608d
nath2712 <61108573+nath2712@users.noreply.github.com>
2021-04-27 11:38:17 +0200

    Add files via upload

---
+++ mastermind/index.php
@@ -0,0 +1,98 @@
+
+<!doctype html>
+<html>
+
+<head>
+
+<meta charset="UTF-8">
+
+<script src="script.js"></script>
+</head>
+
+	<body>
+
+
+		<p > MasterMind textuel </p>
+
+
+
+		<table>
+			<legend> historique des proposition </legend>
+			<tr> 
+				
+					<td > 0 </td>
+					<td > 0 </td>
+					<td > 0 </td>
+					<td > 0 </td>
+				
+			</tr>
+			<tr> 
+				
+					<td > 0 </td>
+					<td > 0 </td>
+					<td > 0 </td>
+					<td > 0 </td>
+				
+			</tr>
+		
+		</table>
+
+
+		<select name ="Premier numéro">
+
+			<option value="1"> 1 </option>
+
+			<option value="2"> 2 </option>
+			<option value="3"> 3 </option>
+
+			<option value="4"> 4 </option>
+		</select> 
+		<select name ="Second numéro">
+
+			<option value="1"> 1 </option>
+
+			<option value="2"> 2 </option>
+			<option value="3"> 3 </option>
+
+			<option value="4"> 4 </option>
+		</select> 
+		<select name ="Troisiéme numéro">
+
+			<option value="1"> 1 </option>
+
+			<option value="2"> 2 </option>
+			<option value="3"> 3 </option>
+
+			<option value="4"> 4 </option>
+		</select> 
+		<select name ="Quatriéme numéro">
+
+			<option value="1"> 1 </option>
+
+			<option value="2"> 2 </option>
+			<option value="3"> 3 </option>
+
+			<option value="4"> 4 </option>
+		</select> 
+		
+		<button id ="button" > Tester la combinaison </button>
+		
+		<p> Liste des nombres utilisé :</p>
+		<ul>
+			<li>1</li>
+			<li>2</li>
+			<li>3</li>
+			<li>4</li>
+
+		</ul>
+	</body>
+
+	<script type = "text/javascript">
+
+		
+
+	</script>
+
+
+
+</html>
---
+++ mastermind/script.js
@@ -0,0 +1,50 @@
+var btn = document.getElementById('button');
+
+btn.onclick= function() ={
+	
+	alerte('Ca marche');
+};
+
+
+window.addEventListener('load',()=>{
+	
+		alerte('Ca marche');
+	document.querySelector('button').addEventListener('click',(event)=>{
+		alerte('Ca marche');
+		
+		
+		
+		
+	});
+});
+
+
+window.addEventListener('load',()=>{
+	
+	
+		let event=$('event').value;
+		let selector=$('selector').value;
+
+		$('btn').addEventListener('click',)()=>{
+		document.querySelector(selector).addEventListener((event) =>{
+		alert('Ca marche');
+
+		
+		
+		});
+	});
+	
+	
+	
+	
+});
+
+
+
+
+
+
+
+
+
+
